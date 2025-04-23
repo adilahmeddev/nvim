9641836408db689d3a9312e2a911b5803509d2fc
Adil Ahmed <adil.ahmed@triptease.com>
2025-04-23 16:27:56 +0100

    modular kickstart, v 0.11.0

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,7 @@
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('n', '<leader>ff', ':Neotree float reveal_force_cwd<cr>', { desc = 'Move focus to the upper window' })
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
