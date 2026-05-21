05b0df8ea43ddc148baeefd8a70ad0ddd978ace1
adil <adilahmeddev@gmail.com>
2026-05-21 19:36:33 +0100

    yep

--- init.lua
+++ init.lua
@@ -91,9 +91,6 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-
-
-vim.o.fileformats = 'unix'
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
